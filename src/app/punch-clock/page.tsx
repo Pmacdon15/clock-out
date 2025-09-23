926cc7ef738460be1d0e760765367bc1a35e1e7a
Patrick MacDonald <patrick@patmac.ca>
2025-09-22 23:11:22 -0600

    some css

--- src/app/punch-clock/page.tsx
+++ src/app/punch-clock/page.tsx
@@ -4,7 +4,7 @@ import { OrganizationSwitcher, SignedIn, UserButton } from "@clerk/nextjs";
 export default function Page() {
     return (
         <div className="flex justify-center p-4 mt-8">
-            <div className="rounded-xl p-1 bg-gradient-to-r from-blue-500 to-cyan-500">
+            <div className="rounded-xl p-1 bg-gradient-to-r from-blue-500 to-cyan-500 w-5/6 mdw-4/6">
                 <div className="flex flex-col gap-4 rounded-xl p-8  bg-black">
                     <SignedIn>
                         <div className="flex text-white justify-center">
--- src/components/ui/headers/layout-header.tsx
+++ src/components/ui/headers/layout-header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
                     </div>
                 </SignedOut>
             </div>
-            <div className="flex items-center justify-center w-4/5 md:w-3/6 p-4 mx-auto rounded-full border-[3px] border-dashed border-blue-400">
+            <div className="flex items-center justify-center w-5/6 md:w-3/6 p-4 mx-auto rounded-full border-[3px] border-dashed border-blue-400">
                 <div className={'flex gap-2 text-4xl font-bold justify-center align-middle items-center'}>Punch Clock App<ClockAlert size={34} /></div>
             </div>
         </div>
