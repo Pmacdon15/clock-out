949719bbd0f08a3ace9d6985f90c4423aaabf1bd
Patrick MacDonald <patrick@patmac.ca>
2025-10-05 14:28:23 -0600

    made reusable component

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,3 +1,4 @@
+import TermsContainer from '@/components/ui/containers/terms-container';
 import fs from 'fs/promises';
 import path from 'path';
 
@@ -11,13 +12,10 @@ export default async function PrivacyPage() {
   const privacyContent = await getPrivacyContent();
 
   return (
-    <div className="min-h-screen bg-background text-foreground p-8">
-      <div className="max-w-3xl mx-auto">
-        <h1 className="text-3xl font-bold mb-6">Privacy Policy</h1>
-        <pre className="whitespace-pre-wrap font-sans">
+    <div className="min-h-screen bg-background text-foreground p-8 w-full">
+      <TermsContainer  typeOfContainer={'Privacy Policy'}> 
           {privacyContent}
-        </pre>
-      </div>
+      </TermsContainer>
     </div>
   );
 }
