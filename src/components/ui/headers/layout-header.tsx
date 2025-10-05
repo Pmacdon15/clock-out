1d6b038a32f4fe92a0177fba3331a6119fb2046e
Patrick MacDonald <patrick@patmac.ca>
2025-10-04 22:39:27 -0600

    css

--- src/components/ui/headers/layout-header.tsx
+++ src/components/ui/headers/layout-header.tsx
@@ -5,20 +5,8 @@ import Link from "next/link";
 export default function Header() {
     return (
         <Link href={"/"}>
-            <div className="w-full flex flex-col">
-                <div className="flex justify-end p-4">
-                    <div>
-                        <SignedOut>
-                            <div className="flex gap-2">
-                                <SignInButton forceRedirectUrl={'punch-clock'} />
-                                <SignUpButton forceRedirectUrl={'punch-clock'} />
-                            </div>
-                        </SignedOut>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center w-5/6 md:w-3/6 p-4 mx-auto rounded-full border-[3px] border-dashed border-blue-400">
-                    <div className={'flex gap-2 text-4xl font-bold justify-center align-middle items-center text-center'}>Clock Out <ClockAlert size={34} /></div>
-                </div>
+            <div className="flex items-center justify-center w-5/6 md:w-3/6 p-4 mx-auto rounded-full border-[3px] border-dashed border-blue-400 mt-8">
+                <div className={'flex gap-2 text-4xl font-bold justify-center align-middle items-center text-center'}>Clock Out <ClockAlert size={34} /></div>
             </div>
         </Link>
     );
