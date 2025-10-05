4e33e97702a2e0fdfd5f2f246598f0088274c25a
Patrick MacDonald <patrick@patmac.ca>
2025-10-05 17:24:38 -0600

    made more components server components

--- src/components/ui/filters/week-selector.tsx
+++ src/components/ui/filters/week-selector.tsx
@@ -1,17 +1,29 @@
 'use client';
 
+import { useRouter } from 'next/navigation'
+
 interface WeekSelectorProps {
   weeks: { label: string, value: string }[];
-  selectedWeek: string;
-  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-export function WeekSelector({ weeks, selectedWeek, onChange }: WeekSelectorProps) {
+export function WeekSelector({ weeks }: WeekSelectorProps) {
+  const router = useRouter();
+
+  const handleWeekChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedWeek = event.target.value;
+    router.push(`/hours-worked/${selectedWeek}`);
+  };
+
   return (
     <div className="mb-4">
       <label htmlFor="week-select" className="mr-2">Filter by week:</label>
-      <select id="week-select" value={selectedWeek} onChange={onChange} className="border p-1 rounded">
-        <option value={""}>Select a Week</option>
+      <select
+        id="week-select"
+        defaultValue={weeks[weeks.length - 1].value}
+        onChange={handleWeekChange}
+        className="border p-1 rounded"
+      >
+        <option value="">Select a Week</option>
         {weeks.map(week => (
           <option key={week.value} value={week.value}>{week.label}</option>
         ))}
