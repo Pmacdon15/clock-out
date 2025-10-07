4937eb44e6fb7462379b92b610f87f8a151422f1
Patrick MacDonald <patrick@patmac.ca>
2025-10-06 21:36:10 -0600

    added abstractions

--- src/app/punch-clock/page.tsx
+++ src/app/punch-clock/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import BorderBox from "@/components/ui/containers/border-box";
 import TimeInput from "@/components/ui/inputs/time-input";
 import { getTimeCard } from "@/lib/DAL/punch-clock";
 import { OrganizationSwitcher, SignedIn, UserButton } from "@clerk/nextjs";
@@ -8,23 +9,21 @@ export default async function Page() {
     const timeCard = await getTimeCard()
     return (
         <div className="flex justify-center p-4 mt-8">
-            <div className="rounded-xl p-1 bg-gradient-to-r from-blue-500 to-cyan-500 w-full md:w-3/6 ">
-                <div className="flex flex-col gap-4 rounded-xl p-8 bg-black">
-                    <SignedIn>
-                        <div className="flex text-white justify-center">
-                            <UserButton />
-                            <OrganizationSwitcher />
-                        </div>
-                        <div className="flex justify-end ">
-                            <Link href="/hours-worked">
-                                <Button>Hours Worked</Button>
-                            </Link>
-                        </div>
-                    </SignedIn>
-                    <TimeInput disabled={!!timeCard} clockInTime={timeCard?.time_in} />
-                    <TimeInput punchOut disabled={!timeCard} clockInTime={timeCard?.time_in} />
-                </div>
-            </div>
+            <BorderBox>
+                <SignedIn>
+                    <div className="flex text-white justify-center">
+                        <UserButton />
+                        <OrganizationSwitcher />
+                    </div>
+                    <div className="flex justify-end ">
+                        <Link href="/hours-worked">
+                            <Button>Hours Worked</Button>
+                        </Link>
+                    </div>
+                </SignedIn>
+                <TimeInput disabled={!!timeCard} clockInTime={timeCard?.time_in} />
+                <TimeInput punchOut disabled={!timeCard} clockInTime={timeCard?.time_in} />
+            </BorderBox>
         </div>
     );
 }
