491a3204be10ab6448e51ae95cf0b998ab8f6a6d
Patrick MacDonald <patrick@patmac.ca>
2025-10-11 15:56:34 -0600

    code clean up

--- src/components/ui/edit-hours/list-item-edit-hours.tsx
+++ src/components/ui/edit-hours/list-item-edit-hours.tsx
@@ -11,14 +11,6 @@ export default function ListItemEditHours({
 	key: number
 	entry: TimeCard
 }) {
-	function formatDateForInput(date: Date) {
-		const year = date.getFullYear()
-		const month = String(date.getMonth() + 1).padStart(2, '0')
-		const day = String(date.getDate()).padStart(2, '0')
-		const hour = String(date.getHours()).padStart(2, '0')
-		const minute = String(date.getMinutes()).padStart(2, '0')
-		return `${year}-${month}-${day}T${hour}:${minute}`
-	}
 	const [editHoursState, setEditHours] = useState(false)
 
 	return (
@@ -27,44 +19,20 @@ export default function ListItemEditHours({
 			key={entry.id}
 		>
 			<div className="col-span-2 lg:col-span-4">
-				{!editHoursState ? (
-					<div className="grid grid-cols-2 gap-4">
-						<p className="text-sm text-gray-500">
-							{new Date(entry.time_in).toLocaleString()}
-						</p>
-						<p className="text-sm text-gray-500">
-							{entry.time_out
-								? new Date(entry.time_out).toLocaleString()
-								: 'Not punched out'}
-						</p>
-					</div>
-				) : (
-					<form className="grid grid-cols-1 lg:grid-cols-3 gap-4">						
-						<Input
-							className="w-full"
-							defaultValue={formatDateForInput(
-								new Date(entry.time_in),
-							)}
-							name="time_in"
-							type="datetime-local"
-						/>
-						<Input
-							className="w-full"
-							defaultValue={
-								entry.time_out
-									? formatDateForInput(
-											new Date(entry.time_out),
-										)
-									: ''
-							}
-							name="time_out"
-							type="datetime-local"
-						/>
-						<EditHoursButton
-							onSuccess={() => setEditHours(false)}
-							punchClockId={entry.id}
-						/>
-					</form>
+				{!editHoursState && entry.time_out && (
+					
+					<DisplayTimes
+						timeIn={entry.time_in}
+						timeOut={entry.time_out}
+					/>
+				)}
+				{editHoursState && entry.time_out && (
+					<EditTimeForm
+						id={entry.id}
+						onSuccess={() => setEditHours(false)}
+						timeIn={entry.time_in}
+						timeOut={entry.time_out}
+					/>
 				)}
 			</div>
 			<div className="col-span-1 lg:col-span-1 mt-4 lg:mt-0 text-left">
@@ -90,4 +58,60 @@ export default function ListItemEditHours({
 			</div>
 		</li>
 	)
+}
+
+function DisplayTimes({ timeIn, timeOut }: { timeIn: Date; timeOut: Date }) {
+	return (
+		<div className="grid grid-cols-2 gap-4">
+			<p className="text-sm text-gray-500">
+				{new Date(timeIn).toLocaleString()}
+			</p>
+			<p className="text-sm text-gray-500">
+				{timeOut
+					? new Date(timeOut).toLocaleString()
+					: 'Not punched out'}
+			</p>
+		</div>
+	)
+}
+
+function EditTimeForm({
+	id,
+	timeIn,
+	timeOut,
+	onSuccess,
+}: {
+	id: number
+	timeIn: Date
+	timeOut: Date
+	onSuccess: () => void
+}) {
+	return (
+		<form className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+			<Input
+				className="w-full"
+				defaultValue={formatDateForInput(new Date(timeIn))}
+				name="time_in"
+				type="datetime-local"
+			/>
+			<Input
+				className="w-full"
+				defaultValue={
+					timeOut ? formatDateForInput(new Date(timeOut)) : ''
+				}
+				name="time_out"
+				type="datetime-local"
+			/>
+			<EditHoursButton onSuccess={() => onSuccess()} punchClockId={id} />
+		</form>
+	)
+}
+
+function formatDateForInput(date: Date) {
+	const year = date.getFullYear()
+	const month = String(date.getMonth() + 1).padStart(2, '0')
+	const day = String(date.getDate()).padStart(2, '0')
+	const hour = String(date.getHours()).padStart(2, '0')
+	const minute = String(date.getMinutes()).padStart(2, '0')
+	return `${year}-${month}-${day}T${hour}:${minute}`
 }
