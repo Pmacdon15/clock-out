09d6fed8498daa79f95920e4d206664b11151558
Patrick MacDonald <patrick@patmac.ca>
2025-10-11 16:05:44 -0600

    code clean up

--- src/components/ui/edit-hours/list-item-edit-hours.tsx
+++ src/components/ui/edit-hours/list-item-edit-hours.tsx
@@ -93,6 +93,15 @@ function EditTimeForm({
 	timeOut?: Date | null
 	onSuccess: () => void
 }) {
+	function formatDateForInput(date: Date) {
+		const year = date.getFullYear()
+		const month = String(date.getMonth() + 1).padStart(2, '0')
+		const day = String(date.getDate()).padStart(2, '0')
+		const hour = String(date.getHours()).padStart(2, '0')
+		const minute = String(date.getMinutes()).padStart(2, '0')
+		return `${year}-${month}-${day}T${hour}:${minute}`
+	}
+
 	return (
 		<form className="grid grid-cols-1 lg:grid-cols-3 gap-4">
 			<Input
@@ -112,13 +121,4 @@ function EditTimeForm({
 			<EditHoursButton onSuccess={() => onSuccess()} punchClockId={id} />
 		</form>
 	)
-}
-
-function formatDateForInput(date: Date) {
-	const year = date.getFullYear()
-	const month = String(date.getMonth() + 1).padStart(2, '0')
-	const day = String(date.getDate()).padStart(2, '0')
-	const hour = String(date.getHours()).padStart(2, '0')
-	const minute = String(date.getMinutes()).padStart(2, '0')
-	return `${year}-${month}-${day}T${hour}:${minute}`
 }
