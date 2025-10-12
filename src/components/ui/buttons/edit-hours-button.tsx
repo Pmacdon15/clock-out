d5d83753af54e2bf541526361289098dcd0f0c04
Patrick MacDonald <patrick@patmac.ca>
2025-10-11 18:07:20 -0600

    trying to pass time zone for converision

--- src/components/ui/buttons/edit-hours-button.tsx
+++ src/components/ui/buttons/edit-hours-button.tsx
@@ -1,32 +1,63 @@
-'use client'
-import { useEditHours } from '@/lib/mutations/edit-hours'
-import { Button } from '../button'
+'use server'
+import { auth } from '@clerk/nextjs/server'
+import { updatePunchClock } from '@/lib/DB/edit-hours'
+import { EditHoursSchema } from '@/lib/zod/edit-hours'
 
-const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+export async function editHours(
+	formData: FormData,
+	punchClockId: number,
+	timeZone: string,
+) {
+	const { orgId } = await auth.protect()
 
-export default function EditHoursButton({
-	punchClockId,
-	onSuccess,
-}: {
-	punchClockId: number
-	onSuccess: () => void
-}) {
-	const { mutate, isPending } = useEditHours({
-		onSuccess: () => {
-			onSuccess()
-		},
+	if (!orgId) {
+		return
+	}
+
+	// Get time_in and time_out from formData
+	const timeIn = formData.get('time_in')?.toString()
+	const timeOut = formData.get('time_out')?.toString()
+
+	if (!timeIn || !timeOut) {
+		return {
+			error: 'Time in and time out are required',
+		}
+	}
+
+	// Convert time to UTC
+	let utcTimeIn: string
+	let utcTimeOut: string
+
+	try {
+		utcTimeIn = new Date(`${timeIn} ${timeZone}`).toISOString()
+		utcTimeOut = new Date(`${timeOut} ${timeZone}`).toISOString()
+	} catch (error) {
+		console.error('Error converting time to UTC:', error)
+		return {
+			error: 'Invalid date or time format',
+		}
+	}
+
+	const validatedFields = EditHoursSchema.safeParse({
+		id: punchClockId,
+		time_in: utcTimeIn,
+		time_out: utcTimeOut,
+		timeZone: timeZone,
 	})
-	return (
-		<Button
-			className="w-full lg:w-auto"
-			disabled={isPending}
-			formAction={(formData: FormData) =>
-				mutate({ formData, punchClockId, timeZone })
-			}
-			type="submit"
-			variant={'outline'}
-		>
-			{!isPending ? 'Save' : 'Saving'}
-		</Button>
-	)
+
+	console.log('time_in', validatedFields.data?.time_in)
+	if (!validatedFields.success) {
+		return {
+			error: validatedFields.error.flatten().fieldErrors,
+		}
+	}
+
+	try {
+		await updatePunchClock(validatedFields.data)
+	} catch (error) {
+		console.error(error)
+		return {
+			error: 'Something went wrong',
+		}
+	}
 }
