61ab47ad105d9ea2d270d86e398d46d9d12ecf48
Patrick MacDonald <patrick@patmac.ca>
2025-10-16 22:27:41 -0600

    stage 3 added start end date selectors

--- src/components/ui/hours-worked/pay-period-selector.tsx
+++ src/components/ui/hours-worked/pay-period-selector.tsx
@@ -1,3 +1,55 @@
-export default function PayPeriodSelector() {
-	return <h1>test</h1>
+import { useSearchParams } from 'next/navigation'
+import {
+	formatDateForInput,
+	useHandleParamChange,
+} from '@/lib/utils/filter-utils'
+import { Input } from '../input'
+
+export default function PayPeriodSelector({
+	startDate,
+	endDate,
+}: {
+	startDate: string
+	endDate: string
+}) {
+	return (
+		<div>
+			<DateSelector date={startDate} />
+			<DateSelector date={endDate} variant="endDate" />
+		</div>
+	)
+}
+
+function DateSelector({
+	date,
+	variant = 'startDate',
+}: {
+	date?: string
+	variant?: 'startDate' | 'endDate'
+}) {
+	const searchParams = useSearchParams()
+	let dateToSet: string | null
+	if (variant === 'startDate') dateToSet = searchParams.get('startDate')
+	else if (variant === 'endDate') dateToSet = searchParams.get('endDate')
+	else dateToSet = date ?? ''
+
+	const handleParamChange = useHandleParamChange()
+	return (
+		<div className="flex w-3/6">
+			<h1 className="w-1/6">
+				{variant === 'startDate' ? 'Start Date' : 'End Date'}
+			</h1>
+			<Input
+				className="w-2/6 [&::-webkit-datetime-edit]:text-white [&::-webkit-calendar-picker-indicator]:invert"
+				defaultValue={
+					dateToSet ? formatDateForInput(new Date(dateToSet)) : ''
+				}
+				name={variant}
+				onChange={(value) =>
+					handleParamChange(variant, String(value), '/hours-worked')
+				}
+				type="datetime-local"
+			/>
+		</div>
+	)
 }
--- src/lib/utils/filter-utils.ts
+++ src/lib/utils/filter-utils.ts
@@ -14,3 +14,12 @@ export function useHandleParamChange() {
 		router.push(`${path}?${params.toString()}`)
 	}
 }
+
+export function formatDateForInput(date: Date) {
+	const year = date.getFullYear()
+	const month = String(date.getMonth() + 1).padStart(2, '0')
+	const day = String(date.getDate()).padStart(2, '0')
+	const hour = String(date.getHours()).padStart(2, '0')
+	const minute = String(date.getMinutes()).padStart(2, '0')
+	return `${year}-${month}-${day}T${hour}:${minute}`
+}
